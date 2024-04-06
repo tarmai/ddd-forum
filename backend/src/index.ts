b1c08c26ee39903498133b1ce3a553a1dbe43577
tarmai <taras.maister@softcery.com>
2024-04-06 15:03:16 +0300

    chore(api): return mock responses from user endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,15 +6,15 @@ app.use(express.json())
 app.use(cors())
 
 app.post('/users', async (req: Request, res: Response) => {
-  // ...
+  res.send([])
 })
 
 app.patch('/users/:userId', async (req: Request, res: Response) => {
-  // ...
+  res.send([])
 })
 
 app.get('/users', async (req: Request, res: Response) => {
-  // ...
+  res.send([])
 })
 
 const port = process.env.PORT || 3000
